7c92506512360fa0c921de071386e9323ed9583c
kang sinuk <ghttttgu@naver.com>
2019-08-23 16:01:19 +0900

    nuclear end

--- SWE_code/nuclear_collision/nuclear.cpp
+++ SWE_code/nuclear_collision/nuclear.cpp
@@ -3,66 +3,53 @@
 #include <iostream>
 using namespace std;
 
-class Atom{
-public:
-    int index, x, y, arrow, energy;
-    Atom(){
-        index = x = y = arrow = energy = 0;
-    }
-    bool operator==(Atom &other){
-        if(x==other.x && y==other.y) return true;
-        return false;
-    }
+struct Atom{
+    int x, y, arrow, energy;
 };
 
-inline int abs(int x){ return x > 0 ? x : -x;}
-
-
 int is_will_collion(Atom atom1, Atom atom2){
-    int ok = 0;
-    if(atom1 == atom2) {
+    if(atom1.arrow == atom2.arrow) return 0;
+
+    if(atom1.x == atom2.x){ // same x
+        if(atom1.y < atom2.y && atom1.arrow==0 && atom2.arrow==1) return atom2.y - atom1.y;
+        if(atom1.y > atom2.y && atom1.arrow==1 && atom2.arrow==0) return atom1.y - atom2.y;
+        return 0;
+    }
+    if(atom1.y == atom2.y){ // same y
+        if(atom1.x < atom2.x && atom1.arrow==3 && atom2.arrow==2) return atom2.x - atom1.x;
+        if(atom1.x > atom2.x && atom1.arrow==2 && atom2.arrow==3) return atom1.x - atom2.x;
         return 0;
     }
 
-    if(atom1.x == atom2.x){ // same x
-        if(atom1.y < atom2.y && atom1.arrow==0 && atom2.arrow==1) ok = 1;
-        else if(atom1.y > atom2.y && atom1.arrow==1 && atom2.arrow==0) ok = 1;
+    int dx = atom1.x-atom2.x;
+    int dy = atom1.y-atom2.y;
+
+    if(dx != dy && dx != -dy) return 0;
+    if(atom1.x<atom2.x){
+        if(atom1.y<atom2.y){
+            if(atom1.arrow==3 && atom2.arrow==1 || atom1.arrow==0 && atom2.arrow==2 ) return -dx;
+            return 0;
+        }
+        if(atom1.arrow==3 && atom2.arrow==0 || atom1.arrow==1 && atom2.arrow==2 ) return -dx;
+        return 0;
     }
-    else if(atom1.y == atom2.y){ // same y
-        if(atom1.x < atom2.x && atom1.arrow==3 && atom2.arrow==2) ok = 1;
-        else if(atom1.x > atom2.x && atom1.arrow==2 && atom2.arrow==3) ok = 1;
-    }
-    else if( abs(atom1.x-atom2.y)==abs(atom1.y-atom2.x) ){
-        if(atom1.x<atom2.x && atom1.y<atom2.y){
-            if(atom1.arrow==3 && atom2.arrow==1 || atom1.arrow==0 && atom2.arrow==2 ) ok=1;
+    else{
+        if(atom1.y<atom2.y) {
+            if(atom1.arrow==0 && atom2.arrow==3 || atom1.arrow==2 && atom2.arrow==1 ) return dx;
+            return 0;
         }
-        else if(atom1.x<atom2.x && atom1.y>atom2.y){
-            if(atom1.arrow==3 && atom2.arrow==0 || atom1.arrow==1 && atom2.arrow==2 ) ok=1;
-        }
-        else if(atom1.x>atom2.x && atom1.y<atom2.y){
-            if(atom1.arrow==0 && atom2.arrow==3 || atom1.arrow==2 && atom2.arrow==1 ) ok=1;
-        }
-        else if(atom1.x>atom2.x && atom1.y>atom2.y){
-            if(atom1.arrow==1 && atom2.arrow==3 || atom1.arrow==2 && atom2.arrow==0 ) ok=1;
-        }
+        if(atom1.arrow==1 && atom2.arrow==3 || atom1.arrow==2 && atom2.arrow==0 ) return dx;
+        return 0;
     }
 
-    return ok;
+    return 0;
 }
 
 int cal_collion(Atom atomList[], int &Num, int &result){
-    #ifdef DEBBUG
-        for(int i=0;i<Num;i++){
-            printf("index : %d , y : %d , x : %d\n",atomList[i].index,atomList[i].y,atomList[i].x);
-        }
-        cout<<"----------------------"<<endl;
-        
-    #endif
-
     int atoms_bomb[1000] = {0};
     for(int i=0; i<Num; i++){
         for (int j=i+1; j<Num; j++){
-            if(atomList[i] == atomList[j]) atoms_bomb[i] = atoms_bomb[j] = 1;
+            if(atomList[i].x == atomList[j].x && atomList[i].y == atomList[j].y) atoms_bomb[i] = atoms_bomb[j] = 1;
         }
     }
     for(int i=0;i<Num;i++){
@@ -75,12 +62,15 @@ int cal_collion(Atom atomList[], int &Num, int &result){
         }
     }
 
-
     int atoms_ok[1000] = {0};
+    int out = (1<<29);
     for(int i=0; i<Num; i++){
         for (int j=i+1; j<Num; j++){
             int ok = is_will_collion(atomList[i], atomList[j]);
-            if(ok){atoms_ok[i] = atoms_ok[j] = 1;}
+            if(ok){
+                atoms_ok[i] = atoms_ok[j] = 1;
+                out = out < ok ? out : ok;
+            }
         }
     }
     for(int i=0;i<Num;i++){
@@ -92,49 +82,41 @@ int cal_collion(Atom atomList[], int &Num, int &result){
         }
     }
 
-    return 0;
+    out = out/2;
+    return out>1 ? out:1;
 }
 
-int move_atom(Atom atomList[], int Num){
+int move_atom(Atom atomList[], int Num, int time=1){
+    int dx[4] = {0,0,-time,time};
+    int dy[4] = {time,-time,0,0};
+
     for(int i=0; i<Num; i++){
-        Atom atom = atomList[i];
-        switch (atom.arrow)
-        {
-            case 0:
-                atomList[i].y++;
-                break;
-            case 1:
-                atomList[i].y--;
-                break;
-            case 2:
-                atomList[i].x--;
-                break;
-            case 3:
-                atomList[i].x++;
-                break;
-        }
+        int arrow = atomList[i].arrow;
+        atomList[i].x+=dx[arrow];
+        atomList[i].y+=dy[arrow];
     }
 }
 
 int cal_main(){
     int Num;
-    Atom atomList[1000]; //index , x , y , arrow , energy
-    
+    Atom atomList[1000]; //x , y , arrow , energy
 
     scanf("%d",&Num);
     for(int i=0;i<Num;i++){
-        atomList[i].index = i;
         scanf("%d",&atomList[i].x);
+        atomList[i].x *= 2;
         scanf("%d",&atomList[i].y);
+        atomList[i].y *= 2;
         scanf("%d",&atomList[i].arrow);
         scanf("%d",&atomList[i].energy);
     }
     //--------------------------------------------------
     int result = 0;
+    int time = 1;
 
     while(Num>0){
-        move_atom(atomList, Num);
-        cal_collion(atomList, Num, result);
+        move_atom(atomList, Num, time);
+        time = cal_collion(atomList, Num, result);
     }
 
     return result;
